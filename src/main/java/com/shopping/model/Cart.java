0af3be7f1c85ab072282fa4ef4f3216f33711475
Rithik <rithikkumar31299@gmail.com>
2021-07-01 23:29:42 +0530

    Initial Commit

---
+++ src/main/java/com/shopping/model/Cart.java
@@ -0,0 +1,16 @@
+package com.shopping.model;
+
+public class Cart {
+
+    private String cartID;
+    private Double cartTotal;
+
+    public void placeOrder(){
+
+    }
+
+    public void cancelOrder(){
+        
+    }
+
+}
